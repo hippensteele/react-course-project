9f3bbd7377109386028532d7508cfce895737ffe
tomh <hippensteele@gmail.com>
2018-11-26 18:52:49 +0000

    Add order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,6 @@ const INGREDIENT_PRICES = {
     meat: 1.3,
     bacon: 0.7
 };
-
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -18,7 +17,16 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    };
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey];
+        }).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        this.setState({purchasable: sum > 0});
     };
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -31,6 +39,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -46,6 +55,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
     render () {
         const disabledInfo = {
@@ -59,6 +69,7 @@ class BurgerBuilder extends Component {
             <Burger ingredients={this.state.ingredients}/>
             <BuildControls
                 disabled={disabledInfo}
+                purchasable={this.state.purchasable}
                 ingredientAdded={this.addIngredientHandler}
                 ingredientRemoved={this.removeIngredientHandler}
                 price={this.state.totalPrice}/>
